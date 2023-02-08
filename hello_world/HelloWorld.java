5959c3c70d01dc45bf212910078633b19e6b97d2
Katiana Xavier <katianaxavierb@gmail.com>
2023-02-08 17:21:36 -0300

    Exercício Hello World!

---
+++ hello_world/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello_world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Turma 61 é top! sz");
+
+	}
+
+}
